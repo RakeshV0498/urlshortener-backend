8c1c0100b8c979163b96fcf9e8a866a74dbafdfc
RakeshV0498 <rockr4674@gmail.com>
2024-06-18 13:55:20 +0530

    Implemented Forgot Password and Reset Password API

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import mongoConnect from "./db-utils/mongoConnect.js";
 import registerRouter from "./routes/auth/register.js";
 import loginRouter from "./routes/auth/login.js";
+import forgotPassRouter from "./routes/auth/forgotPassword.js";
+import resetPassRouter from "./routes/auth/resetPassword.js";
 
 const server = express();
 const port = 8100;
@@ -19,6 +21,8 @@ mongoConnect();
 
 server.use("/register", registerRouter);
 server.use("/login", loginRouter);
+server.use("/forgot-password", forgotPassRouter);
+server.use("/reset-password", resetPassRouter);
 
 server.listen(port, () => {
   console.log(`${Date().toString()} - server listening on port ${port}`);
