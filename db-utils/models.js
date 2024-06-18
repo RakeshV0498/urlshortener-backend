213ab8bace63db8fcc00e5e3edde03cb2da7cb80
RakeshV0498 <rockr4674@gmail.com>
2024-06-18 09:24:44 +0530

    Created user model

---
+++ db-utils/models.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  id: {
+    type: String,
+    required: "true",
+  },
+  email: {
+    type: String,
+    required: "true",
+  },
+  firstName: {
+    type: String,
+    required: "true",
+  },
+  lastName: {
+    type: String,
+    required: "true",
+  },
+  password: {
+    type: String,
+    required: "true",
+  },
+  active: {
+    type: Boolean,
+    default: false,
+  },
+  resetToken: {
+    type: String,
+    default: null,
+  },
+  resetTokenExpiry: {
+    type: Date,
+    default: null,
+  },
+});
+
+const userModel = new model("user", userSchema, "users");
+
+export { userModel };
