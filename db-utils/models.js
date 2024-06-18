1a7909fe34fb8c1834c7d78815437b048c1dd4d6
RakeshV0498 <rockr4674@gmail.com>
2024-06-18 10:27:10 +0530

    Updated usermodel

--- db-utils/models.js
+++ db-utils/models.js
@@ -25,6 +25,10 @@ const userSchema = new Schema({
     type: Boolean,
     default: false,
   },
+  verifyToken: {
+    type: String,
+    default: null,
+  },
   resetToken: {
     type: String,
     default: null,
