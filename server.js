ec772ce56333e1c24808fb3e4e2fa3c204b880b0
RakeshV0498 <rockr4674@gmail.com>
2024-06-18 09:19:29 +0530

    Created and implemented server

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const server = express();
+const port = 8100;
+
+dotenv.config();
+
+server.use(express.json());
+server.use(cors());
+
+server.listen(port, () => {
+  console.log(`${Date().toString()} - server listening on port ${port}`);
+});
