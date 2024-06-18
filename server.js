0186c5d0197ccbc7470c8e4c90c82093852994b9
RakeshV0498 <rockr4674@gmail.com>
2024-06-18 09:29:34 +0530

    Connnected with Mongodb

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import mongoConnect from "./db-utils/mongoConnect.js";
+
 const server = express();
 const port = 8100;
 
 dotenv.config();
 
 server.use(express.json());
+
 server.use(cors());
+
+mongoConnect();
 
 server.listen(port, () => {
   console.log(`${Date().toString()} - server listening on port ${port}`);
