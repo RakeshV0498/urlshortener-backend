e720b3e6b26641f7599462c90f36c7be3e7930e0
RakeshV0498 <rockr4674@gmail.com>
2024-06-18 18:24:48 +0530

    Updated Authorization to shorten url

--- routes/urlShort.js
+++ routes/urlShort.js
@@ -1,14 +1,21 @@
 import express from "express";
 import { urlModel } from "../db-utils/models.js";
 import { nanoid } from "nanoid";
+import { authenticateUser } from "../server.js";
 
 const urlRouter = express.Router();
 
-urlRouter.post("/shorten", async (req, res) => {
+urlRouter.post("/shorten", authenticateUser, async (req, res) => {
   const { longURL } = req.body;
 
   const shortURL = nanoid(7);
   const id = nanoid(10);
+
+  console.log(req.body);
+
+  if (!longURL) {
+    res.status(404).send({ msg: "Error Creating short URL, Please try again" });
+  }
 
   try {
     const url = await urlModel.create({ id, longURL, shortURL });
@@ -16,7 +23,7 @@ urlRouter.post("/shorten", async (req, res) => {
     return res.send({ shortURL, id, msg: "URL shortned" });
   } catch (error) {
     console.error(error);
-    res.status(500).send({ msg: "Error Creating short URL" });
+    res.status(500).send({ msg: "Something went wrong, please try again" });
   }
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
 import mongoConnect from "./db-utils/mongoConnect.js";
 import registerRouter from "./routes/auth/register.js";
@@ -19,6 +20,27 @@ server.use(express.json());
 server.use(cors());
 
 mongoConnect();
+
+// Middleware to verify JWT token
+export const authenticateUser = (req, res, next) => {
+  // Get token from headers, query parameters, cookies, or wherever you're sending it from the frontend
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+    // Verify the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRETKEY || "");
+    console.log(decoded);
+    // Attach user object to request
+    next(); // Call next middleware
+  } catch (error) {
+    console.error("JWT Verification Error:", error);
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+};
 
 server.use("/register", registerRouter);
 server.use("/login", loginRouter);
